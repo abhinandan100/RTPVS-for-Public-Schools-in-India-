6d286a0219972458203b82ecacb0d3c6e7bdb4d8
abhinandan100 <80942390+abhinandan100@users.noreply.github.com>
2024-07-27 21:13:27 +0530

    Delete MatlabStartSimulation.m

--- MatlabStartSimulation.m
+++
@@ -1,9 +0,0 @@
-function [myErr, matlab_simulation_variables]=MatlabStartSimulation(simulation_parameters)
-myErr.error_description='';
-myErr.severity_code='';
-
-%Initialize user-defined simulation variables. We can use these throughout
-%the simulation for dispatch decisions or to generate errors at the end of simulation.
-matlab_simulation_variables.total_energy_test=0;
-matlab_simulation_variables.gen1_CO=0;
-end
